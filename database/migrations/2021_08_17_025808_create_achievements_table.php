664270513f1f997fd49f30168b1ad477f7424015
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-17 14:52:35 +0900

    update: set primary key(s) to achievements table

--- database/migrations/2021_08_17_025808_create_achievements_table.php
+++ database/migrations/2021_08_17_025808_create_achievements_table.php
@@ -16,6 +16,7 @@ class CreateAchievementsTable extends Migration
         Schema::create('achievements', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
             $table->foreignId('task_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->primary(['user_id','task_id']);
             $table->timestamps();
         });
     }
