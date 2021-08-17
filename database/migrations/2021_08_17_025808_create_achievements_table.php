fbb6e61ac0f4ff7c58e0378d56cd97da8d23a4ba
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-17 12:00:46 +0900

    add: migaration file for achievements

---
+++ database/migrations/2021_08_17_025808_create_achievements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAchievementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->foreignId('task_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievements');
+    }
+}
