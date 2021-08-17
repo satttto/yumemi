d959dcd1bbfd3931708c93d4d604e633e05522f0
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-17 11:57:07 +0900

    add: migration files for tasks and related tables

---
+++ database/migrations/2021_08_17_025217_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('level_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->foreignId('category_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
