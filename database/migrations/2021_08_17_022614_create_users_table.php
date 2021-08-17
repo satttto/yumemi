04c516ba49e54be48ab142e62e3f5b791d1b2ac6
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-17 11:44:32 +0900

    add: migration files for users and roles

--- database/migrations/2021_08_17_022614_create_users_table.php
+++ database/migrations/2021_08_17_022614_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('role_id')->constrained()->onUpdate('RESTRICT')->onDelete('RESTRICT');
             $table->timestamps();
         });
     }
