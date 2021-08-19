761a92fd338769b93643cf805dae096d4764604a
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-19 16:10:43 +0900

    [WIP] add: achievements update for a user

--- app/Http/Controllers/Api/RimoTatsu/AchievementController.php
+++ app/Http/Controllers/Api/RimoTatsu/AchievementController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Api\RimoTatsu;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+/** 
+ * status code
+ * see https://gist.github.com/jeffochoa/a162fc4381d69a2d862dafa61cda0798
+ */
+use \Symfony\Component\HttpFoundation\Response as Status;
 use App\Models\Achievement;
 
 class AchievementController extends Controller
@@ -11,7 +16,7 @@ class AchievementController extends Controller
 
     public function index(Request $request)
     {
-        // テスト用。実際にはAuth::user()->idとしてidを取得
+        // TODO: 実際にはAuth::user()->idとしてidを取得
         $userId = 2; 
 
         // ログインユーザーの権限(role)によってuser_idを含めたり含めなかったり?
@@ -28,17 +33,24 @@ class AchievementController extends Controller
 
     public function update(Request $request)
     {
-        // テスト用。実際にはAuth::user()->idとしてidを取得
+        // TODO: 実際にはAuth::user()->idとしてidを取得
         $userId = 2; 
 
+        // If the user has already taken part in vote, no change will be made.
+        if (!$this->isEditable($userId)) {
+            return response()->error('Not Editable', Status::HTTP_BAD_REQUEST);
+        }
+
+        // TODO: 本当にtask_idが存在するかの確認
         // TODO: 全てdeleteして、新しくinsert
-        
+
+
         return response()->success();
     }
 
     // TODO: votesに参加しているかどうかによって、true/falseを返す
     private function isEditable($userId)
     {
-        return true;
+        return false;
     }
 }
