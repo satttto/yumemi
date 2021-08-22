cb4d8e9b49c82da9dcb193681a8644c106c2ba72
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-22 14:26:41 +0900

    add: formRequest for voting

--- app/Http/Controllers/Api/RimoTatsu/VoteController.php
+++ app/Http/Controllers/Api/RimoTatsu/VoteController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api\RimoTatsu;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\VoteRequest;
+use Illuminate\Http\Request;
+use \Symfony\Component\HttpFoundation\Response as Status; // see details see https://gist.github.com/jeffochoa/a162fc4381d69a2d862dafa61cda0798
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\QueryException;
 use App\Services\VoteService;
-use Illuminate\Http\Request;
-use \Symfony\Component\HttpFoundation\Response as Status; // see details see https://gist.github.com/jeffochoa/a162fc4381d69a2d862dafa61cda0798
 
 
 
@@ -40,24 +41,15 @@ class VoteController extends Controller
     /**
      * 宝くじに参加する
      */
-    public function vote(Request $request)
+    public function vote(VoteRequest $request)
     {
         //TODO: Auth::user()->id
         $userId = 1;
 
-        // TODO: answerのバリデーション
-        try {
-            $request->validate([
-                'answer' => 'required|integer|min:1'
-            ]);
-        } catch (ValidationException $e) {
-            return response()->error('validation error', Status::HTTP_UNPROCESSABLE_ENTITY);
-        }
-
         // ユーザーが投票可能かどうかの判定
         try {
             if (!$this->voteService->isVotable($userId)) {
-                return response()->success('Not votable');
+                return response()->error('Not Votable', Status::HTTP_BAD_REQUEST);
             }
         } catch (QueryException $e) {
             return response()->error('Bad query', Status::HTTP_BAD_REQUEST);
