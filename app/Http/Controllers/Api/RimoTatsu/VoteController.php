2035101f3880d4319ab5a92691155300a76738c3
Satoshi Sugai <s_sugai@yumemi.co.jp>
2021-08-24 12:10:20 +0900

    fix: get winner validation and status code

--- app/Http/Controllers/Api/RimoTatsu/VoteController.php
+++ app/Http/Controllers/Api/RimoTatsu/VoteController.php
@@ -8,16 +8,19 @@ use Illuminate\Http\Request;
 use \Symfony\Component\HttpFoundation\Response as Status; // see details see https://gist.github.com/jeffochoa/a162fc4381d69a2d862dafa61cda0798
 use Illuminate\Database\QueryException;
 use App\Services\VoteService;
+use App\Services\RoleService;
 use Illuminate\Support\Facades\Auth;
 
 
 class VoteController extends Controller
 {
     private $voteService;
+    private $roleService;
 
-    public function __construct(VoteService $voteService)
+    public function __construct(VoteService $voteService, RoleService $roleService)
     {
         $this->voteService = $voteService;
+        $this->roleService = $roleService;
     }
 
     /**
@@ -67,8 +70,21 @@ class VoteController extends Controller
      */
     public function getWinner(Request $request)
     {
-        [$winner, $answer] = $this->voteService->getWinner();
-        return $winner ? response()->success('success', ['user' =>  $winner, 'answer' => $answer]) :
-                               response()->error('failed to fetch');
+        // 管理者であるかどうか確認する(Noの場合はエラー)
+        try {
+            if (!$this->roleService->isAdmin(Auth::user()->role_id)) {
+                return response()->error('No permission', Status::HTTP_BAD_REQUEST);
+            }
+        } catch(QueryException $e) {
+            return response()->error('DB error', Status::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    
+        // 優勝者の取得
+        try {
+            [$winner, $answer] = $this->voteService->getWinner();
+            return response()->success('success', ['user' =>  $winner, 'answer' => $answer]);
+        } catch(QueryException $e) {
+            return response()->error('DB error', Status::HTTP_INTERNAL_SERVER_ERROR);
+        };
     }
 }
